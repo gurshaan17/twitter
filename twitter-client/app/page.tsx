c55b702efce0183ce52cc664e0c684c4073489f4
gurshaan17 <gurshaansinghd@gmail.com>
2024-07-11 22:03:53 +0530

    frontend done for a while

--- twitter-client/app/page.tsx
+++ twitter-client/app/page.tsx
@@ -1,8 +1,9 @@
 import FeedCard from "@/components/FeedCard";
 import Image from "next/image";
 import React from "react";
-import { BiHash, BiHomeCircle, BiUser } from "react-icons/bi";
+import { BiHash, BiHomeCircle, BiMoney, BiUser } from "react-icons/bi";
 import { BsBell, BsBookmark, BsTwitter, BsEnvelope } from "react-icons/bs";
+import { SlOptions } from "react-icons/sl";
 
 
 interface TwitterSidebar{
@@ -28,8 +29,14 @@ const sidebarItems : TwitterSidebar[] = [
     title:'Bookmark',
     icon:<BsBookmark/>
   },{
+    title:'Twitter Blue',
+    icon:<BiMoney/>
+  },{
     title:'Profile',
     icon:<BiUser/>
+  },{
+    title:'More',
+    icon:<SlOptions/>
   }
 ] 
 
@@ -38,20 +45,33 @@ export default function Home() {
   return (
     <div>
       <div className="grid grid-cols-12 h-screen w-screen px-56">
-          <div className="col-span-3 pt-8 ">
-            <div className="text-4xl h-fit w-fit rounded-full p-2 hover:bg-gray-700">
+          <div className="col-span-3 pt-4 ">
+            <div className="text-3xl h-fit w-fit rounded-full p-2 hover:bg-gray-700">
               <BsTwitter/>
             </div>
             <div className="mt-5 text-2xl pr-4">
               <ul>
-                {sidebarItems.map(item=><li className="flex justify-start items-center gap-4 hover:bg-gray-700 w-fit rounded-3xl px-4 py-2 mt-3" key={item.title}><span>{item.icon}</span><span>{item.title} </span></li>)}
+                {sidebarItems.map(item=>
+                <li className="flex justify-start items-center gap-4 hover:bg-gray-700 w-fit rounded-3xl px-3 py-2 mt-3" key={item.title}>
+                  <span className="text-3xl">
+                    {item.icon}
+                  </span>
+                  <span>
+                    {item.title}
+                  </span>
+                </li>)}
               </ul>
               <div className="text-xl font-semibold">
-                <button className="bg-[#1d9bf0] p-4 rounded-full w-[90%] mt-7 hover:bg-sky-500">Tweet</button>
+                <button className="bg-[#1d9bf0] p-3 rounded-full w-[90%] mt-7 hover:bg-sky-500">Tweet</button>
               </div>
             </div>
           </div>
-          <div className="col-span-6 border-r-[1px] border-l-[1px] border-slate-500">
+          <div className="col-span-6 border-r-[1px] border-l-[1px] border-gray-900 h-screen overflow-scroll">
+            <FeedCard/>
+            <FeedCard/>
+            <FeedCard/>
+            <FeedCard/>
+            <FeedCard/>
             <FeedCard/>
           </div>
           <div className="col-span-3">nigger</div>
--- twitter-client/components/FeedCard.tsx
+++ twitter-client/components/FeedCard.tsx
@@ -1,11 +1,26 @@
 import Image from 'next/image'
+import { AiOutlineHeart } from 'react-icons/ai';
+import { BiMessageRounded, BiUpload } from 'react-icons/bi';
+import { FaRetweet } from 'react-icons/fa';
 
 
 const FeedCard = () => {
-    return <div>
-        <div className="grid grid-cols-12">
-            <div className="col-span-2"><Image src='https://avatars.githubusercontent.com/u/140047039?v=4' height={50} width={50}  alt='no user'/></div>
-            <div className="col-span-10 ">there</div>
+    return <div className='p-5 border border-gray-900 border-r-0 border-l-0  hover:bg-gray-900 transition-all'>
+        <div className="grid grid-cols-12 gap-2">
+            <div className="col-span-1"><Image src='https://avatars.githubusercontent.com/u/140047039?v=4' height={50} width={50}  alt='no user'/></div>
+            <div className="col-span-11 pl-3 ">
+                <div className='text-xl'>
+                    Gurshaan Singh
+                </div>
+                <p className='text-md '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium possimus ducimus nesciunt consequatur odio. Quod itaque placeat voluptas cupiditate culpa. Ullam mollitia sunt velit voluptatem deserunt, voluptate doloremque nostrum explicabo sint doloribus odit obcaecati. Necessitatibus.</p>
+                <div className='flex justify-evenly mt-5 '>
+                    <div><BiMessageRounded/></div> 
+                    <div><FaRetweet/></div>
+                    <div><AiOutlineHeart/></div>
+                    <div><BiUpload/></div>
+                </div>
+            </div>
+            
         </div>
     </div>
 }
